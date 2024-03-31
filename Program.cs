74a7a80be61c22bb6f65af150d1eb159634e0913
Yazid Al Ghozali <54970485+yazidalg@users.noreply.github.com>
2024-04-01 01:26:34 +0700

    JSON Serialize Al (#5)
    * add json serialize
    
    * Update tpmod7.csproj
    
    * add kuliahmahasiswa1302223047 class

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿using System;
 
-var dataMahasiswa = new DataMahasiswa1302223083();
-var datakelas=new KuliahMahasiswa1302223083();
+var dataMahasiswa = new DataMahasiswa1302223032();
+var datakelas = new KuliahMahasiswa1302223032();
 
 datakelas.ReadJSON();
 dataMahasiswa.ReadJSON();
+
+var dataMahasiswa1302223047 = new DataMahasiswa1302223047();
+var dataMatkul = new KuliahMahasiswa1302223047();
+
+dataMahasiswa1302223047.ReadJSON();
+dataMatkul.ReadJSON();
+
+var dataMahasiswa1302223083 = new DataMahasiswa1302223083();
+var datakelas1302223083=new KuliahMahasiswa1302223083();
+
+datakelas1302223083.ReadJSON();
+dataMahasiswa1302223083.ReadJSON();
+
+Console.ReadLine();
+
