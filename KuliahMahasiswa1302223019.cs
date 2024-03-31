c7093dc261a49dc9201c5021498c3bc0ca6137cd
purianagatha <purianagatha@gmail.com>
2024-04-01 05:19:40 +0700

    Menambahkan Class KuliahMahasiswa1302223019

---
+++ KuliahMahasiswa1302223019.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using static KuliahMahasiswa1302223019;
+internal class KuliahMahasiswa1302223019
+{
+    public class KuliahMahasiswa
+    {
+        [JsonPropertyName("courses")]
+        public Course[] Courses { get; set; }
+    }
+    public class Course
+    {
+        [JsonPropertyName("code")]
+        public String code { get; set; }
+        [JsonPropertyName("name")]
+        public String name { get; set; }
+    }
+
+    public void ReadJSON()
+    {
+        string filePath = "tp7_2_1302223019.json";
+
+        string jsonText = File.ReadAllText(filePath);
+
+        KuliahMahasiswa KuliahMahasiswa = JsonSerializer.Deserialize<KuliahMahasiswa>(jsonText);
+
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+
+        for (int i = 0; i < KuliahMahasiswa.Courses.Length; i++)
+        {
+            Console.WriteLine($"MK {i + 1} {KuliahMahasiswa.Courses[i].code} - {KuliahMahasiswa.Courses[i].name}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,5 +4,8 @@
     {
         DataMahasiswa1302223019 mahasiswa1302223019 = new DataMahasiswa1302223019();
         mahasiswa1302223019.ReadJSON();
+
+        KuliahMahasiswa1302223019 kuliah1302223019 = new KuliahMahasiswa1302223019();
+        kuliah1302223019.ReadJSON();
     }
 }
