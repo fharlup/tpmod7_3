c47de643e9d1056ff6ae428ee8683a03f0e98556
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-04-01 03:16:20 +0700

    class Mahasiswa

---
+++ KuliahMahasiswa_1302220143.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public class KuliahMahasiswa1302220143
+{
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public List<Course> courses { get; set; }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("tp7_2_1302223083.json");
+        var datakelas = JsonSerializer.Deserialize<KuliahMahasiswa1302220143>(json);
+
+        foreach (var item in datakelas.courses)
+        {
+            Console.WriteLine($"Code: {item.code}, Name: {item.name}");
+        }
+    }
+}
