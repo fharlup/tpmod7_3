b63062442b23ca68a2976623962691123e5eb203
fharlup <35958282+fharlup@users.noreply.github.com>
2024-03-31 21:27:44 +0700

    commite ke 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
 var dataMahasiswa = new DataMahasiswa1302223032();
+var datakelas=new KuliahMahasiswa1302223032();
+
+datakelas.ReadJSON();
 dataMahasiswa.ReadJSON();
 
 Console.ReadLine();
